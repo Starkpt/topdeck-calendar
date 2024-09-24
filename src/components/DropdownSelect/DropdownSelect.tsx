19cda965574f5fd546cef3720ef7f5fa41789c9a
Frederico Ramos <frederico.jlr@gmail.com>
2024-09-24 20:12:30 +0100

    added more game options to dropdown component

--- src/components/DropdownSelect/DropdownSelect.tsx
+++ src/components/DropdownSelect/DropdownSelect.tsx
@@ -26,13 +26,38 @@ const dotStyles: StylesConfig = {
 };
 
 export default function DropdownSelect(props: Props) {
+  const games = [
+    { label: "Dragon Ball Super W", value: "dbsw", image: "/dbsw.png" },
+    { label: "Dragon Ball Super B", value: "dbsb", image: "/dbsb.png" },
+    { label: "Magic: the Gathering", value: "mtg", image: "/mtg.png" },
+    { label: "Flesh & Blood", value: "fab", image: "/fab.png" },
+    { label: "Yu-Gi-Oh!", value: "ygo", image: "/ygu.png" },
+    { label: "Vanguard", value: "vng", image: "/vng.png" },
+    { label: "One Piece", value: "op", image: "/op.png" },
+    { label: "Pokémon", value: "pkm", image: "/pkm.png" },
+    { label: "Lorcana", value: "lcn", image: "/lcn.png" },
+  ];
+
   return (
     <Select
       {...props}
       menuPortalTarget={document.body}
       styles={dotStyles}
+      options={games}
+      formatOptionLabel={(game) => (
+        <div
+          className="game-option"
+          style={{
+            height: "80px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <img src={game?.image} style={{ height: "60px" }} alt="game-image" />
+        </div>
+      )}
+      formatGroupLabel={(e) => <p>{e.label}</p>}
       // defaultValue={colourOptions[2]}
-      options={[{ label: <img src={MtG} />, value: "test", image: "MtG.png" }]}
     />
   );
 }
